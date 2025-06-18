84794b8bb03b2aa1efc8fa40756c921909bcc92e
Sanket Nikam <sanketn734@gmail.com>
2025-06-19 00:43:40 +0530

    Fix: use /tmp/logs for Hugging Face logging

--- src/mlProject/__init__.py
+++ src/mlProject/__init__.py
@@ -7,7 +7,8 @@ import logging
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 
-log_dir = "logs"
+# log_dir = "logs"
+log_dir = os.path.join("/tmp", "logs") # Always use a path relative to a writable directory
 log_filepath = os.path.join(log_dir, "running_logs.log")
 os.makedirs(log_dir, exist_ok=True)
 
